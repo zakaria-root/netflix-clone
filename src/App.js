49cd1c7e06f8deb55c79bc72a49ea8921f636204
zakaria-root <elmourtazakezia@gmail.com>
2022-02-11 14:17:49 +0000

    add the headerMovie component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import HeaderMovie from "./components/HeaderMovie/HeaderMovie";
+import Row from "./components/Row/Row";
+import request from "./request";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi hi celiver coder</h1>
+      <HeaderMovie fetchURL={request.fetchNetflixOriginals} />
+      <Row title="NETFLIX ORIGINAL" fetchURL={request.fetchNetflixOriginals} />
+      <Row title="Trending now" fetchURL={request.fetchTrending} />
+      <Row title="Top reader" fetchURL={request.fetchTopReader} />
+      <Row title="Action" fetchURL={request.fetchActionMovies} />
+      <Row title="Romence" fetchURL={request.fetchRomanceMovies} />
+      <Row title="Comedy" fetchURL={request.fetchComedyMovies} />
+      <Row title="Horrordiscover" fetchURL={request.fetchHorrorMovies} />
+      <Row title="Docemtaries" fetchURL={request.fetchDocumentaries} />
     </div>
   );
 }
